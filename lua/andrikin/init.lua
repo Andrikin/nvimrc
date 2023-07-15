f83b23ac50df48e453410ac4b20d7fc2ca9afcf7
Andrikin <contasecretaalternativa@gmail.com>
2023-07-15 01:10:26 -0300

    lua: configurancao em Lua!

---
+++ lua/andrikin/init.lua
@@ -0,0 +1,10 @@
+-- Chamar todos os arquivos
+require('andrikin.lazy')
+require('andrikin.color')
+require('andrikin.lualine')
+require('andrikin.options')
+require('andrikin.maps')
+require('andrikin.autocmds')
+require('andrikin.commands')
+require('andrikin.lsp')
+
