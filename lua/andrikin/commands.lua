0334bcb78fca2916a17546c37b43b9bea1bda315
Andrikin <contasecretaalternativa@gmail.com>
2023-08-06 17:50:38 -0300

    feat: Cmus command
    Manage cmus-remote commands in Nvim!

--- lua/andrikin/commands.lua
+++ lua/andrikin/commands.lua
@@ -1,2 +1,32 @@
 -- CUSTOM COMMANDS
 vim.api.nvim_create_user_command('HexEditor', '%!xxd', {})
+vim.api.nvim_create_user_command('Cmus', 'silent !cmus-remote <args>', {
+	nargs = '+',
+	complete = function(ArgLead, CmdLine, CursorPos)
+		return {
+			'--play',
+			'-p',
+			'--pause',
+			'-u',
+			'--stop',
+			'-s',
+			'--next',
+			'-n',
+			'--prev',
+			'-r',
+			'--repeat',
+			'-R',
+			'--clear',
+			'-c',
+			'--shuffle',
+			'-S',
+			'--file',
+			'-f',
+			'-Q', -- player status information
+			'--queue',
+			'-q',
+			'--raw', -- insert command
+			'-C', -- insert command
+		}
+	end
+})
