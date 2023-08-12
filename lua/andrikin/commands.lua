dab56c38400aa1ba31e381e8b3ad13a18a7ccb14
Andrikin <contasecretaalternativa@gmail.com>
2023-08-12 17:01:20 -0300

    fix: fd spaces not escaped
    Espaços precisam ser escaped para o comando ser executado pelo
    cmus-remote

--- lua/andrikin/commands.lua
+++ lua/andrikin/commands.lua
@@ -7,11 +7,14 @@ if fn.executable('fd') ~= 1 then
 	-- Criar uma função para enviar erros
 	print('Não foi encontrado programa "fd". Realize a instalação do programa!')
 end
+local escapespace = function(s)
+	return fn.substitute(s, [[\s]], [[\\ ]], 'g')
+end
 api.nvim_create_user_command('HexEditor', '%!xxd', {})
 api.nvim_create_user_command('Cmus', 'silent !cmus-remote <args>', {
 	nargs = '+',
 	complete = function(lead, cmd, pos)
-		local DIR_MUSICAS = [[~/music/]]
+		local DIR_MUSICAS = vim.env.HOME .. [[/music/]]
 		local cmus_options = {
 			'--play', '-p',
 			'--pause', '-u',
@@ -26,18 +29,21 @@ api.nvim_create_user_command('Cmus', 'silent !cmus-remote <args>', {
 			'--queue', '-q',
 			'--raw', '-C', -- insert command
 		}
-		local sub_prefix = '<barra_espaco>' -- remover '\ ' como separador de argumentos
+		local sub_prefix = '<barra_espaco>' -- Removendo '\ '
 		cmd = fn.substitute(cmd, [[\\\s]], sub_prefix, 'g')
 		local args = fn.split(cmd, [[\s]])
+		for i, arg in ipairs(args) do -- Recolocando '\ '
+			args[i] = fn.substitute(arg, sub_prefix, [[\\ ]], 'g')
+		end
 		local n = table.getn(args)
 		if lead == '' then -- Quando estiver dando entrada em novo argumento
 			-- Verificar se existe a opção de 'queue'
 			if args[n] == '--queue' or args[n] == '-q' then
 				local dirs = {}
-				local ls = fn.systemlist('fd -p --type directory ' .. DIR_MUSICAS)
+				local ls = fn.systemlist({'ls', DIR_MUSICAS})
 				table.insert(dirs, DIR_MUSICAS)
 				for _, diretorio in ipairs(ls) do -- adicionar '~/' no início de todas as opções 
-					table.insert(dirs, '~/' .. diretorio)
+					table.insert(dirs, DIR_MUSICAS .. diretorio)
 				end
 				return dirs
 			end
@@ -45,9 +51,9 @@ api.nvim_create_user_command('Cmus', 'silent !cmus-remote <args>', {
 		end
 		if args[n - 1] == '--queue' or args[n - 1] == '-q' then -- Entrada parcial para '--queue'
 			local dirs = {}
-			local ls = fn.systemlist('fd --glob -p --type directory ' .. args[n] .. '*')
+			local ls = fn.systemlist({'fd', '--glob', '--full-path', '--type', 'directory', args[n] .. '*', DIR_MUSICAS})
 			for _, diretorio in ipairs(ls) do -- adicionar '~/'
-				table.insert(dirs, '~/' .. diretorio)
+				table.insert(dirs, escapespace(diretorio))
 			end
 			return dirs
 		end
