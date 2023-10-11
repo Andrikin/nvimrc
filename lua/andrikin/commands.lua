6d8d4c089d37ba682eb47037c31e0d122df88225
Andrikin <contasecretaalternativa@gmail.com>
2023-10-11 02:53:00 -0300

    refac: funcao para complete comando Cmus
    Refatorado função para completar autopreenchimento do comando Cmus.
    Fix: Argumentos de caminhos de diretórios possuem escape para espaços
    e autopreenchimento de diretórios.

--- lua/andrikin/commands.lua
+++ lua/andrikin/commands.lua
@@ -1,72 +1,59 @@
 -- CUSTOM COMMANDS
 local fn = vim.fn
 local api = vim.api
--- WIP
--- Verificando dependência -- fd
-if fn.executable('fd') ~= 1 then
-	-- Criar uma função para enviar erros
-	print('Não foi encontrado programa "fd". Realize a instalação do programa!')
+
+local FILLME = function(argumento, comando, posicao)
+	local MUSICAS = vim.env.HOME .. [[/music/]]
+	local opts = {
+		'--play', '-p',
+		'--pause', '-u',
+		'--stop', '-s',
+		'--next', '-n',
+		'--prev', '-r',
+		'--repeat', '-R',
+		'--clear', '-c', -- Clear playlist, library (-l) or play queue (-q).
+		'--shuffle', '-S',
+		'--file', '-f',
+		'-Q', -- player status information
+		'--queue', '-q',
+		'--raw', '-C', -- insert command
+	}
+	if not string.match(comando, '-') then -- nenhum comando inserido, retonar todos os comandos
+		return opts
+	end
+	if string.match(argumento, '^-') then -- completar o comando atual
+		local prompt = {}
+		for _, cmd in ipairs(opts) do
+			if string.match(cmd, argumento) then
+				table.insert(prompt, cmd)
+			end
+		end
+		return prompt
+	end
+	local diretorios = {}
+	local ls = fn.systemlist({'ls', MUSICAS})
+	if not string.match(comando, MUSICAS) then -- quando nenhum diretório foi escolhido
+		if string.match(comando, '([-]?[-][qQ])') then -- checando se '--queue', '-q' ou '-Q' no comando
+			table.insert(diretorios, MUSICAS)
+			for _, diretorio in ipairs(ls) do -- adicionar '~/' no início de todas as opções 
+				diretorio = fn.fnameescape(MUSICAS .. diretorio)
+				table.insert(diretorios, diretorio)
+			end
+		end
+	else
+		for _, cmd in ipairs(ls) do
+			cmd = fn.fnameescape(MUSICAS .. cmd)
+			if string.match(cmd, argumento) then
+				table.insert(diretorios, cmd)
+			end
+		end
+	end
+	return diretorios
 end
-local escapespace = function(s)
-	return fn.substitute(s, [[\s]], [[\\ ]], 'g')
-end
+
+
 api.nvim_create_user_command('HexEditor', '%!xxd', {})
 api.nvim_create_user_command('Cmus', 'silent !cmus-remote <args>', {
 	nargs = '+',
-	complete = function(lead, cmd, pos)
-		local DIR_MUSICAS = vim.env.HOME .. [[/music/]]
-		local cmus_options = {
-			'--play', '-p',
-			'--pause', '-u',
-			'--stop', '-s',
-			'--next', '-n',
-			'--prev', '-r',
-			'--repeat', '-R',
-			'--clear', '-c', -- Clear playlist, library (-l) or play queue (-q).
-			'--shuffle', '-S',
-			'--file', '-f',
-			'-Q', -- player status information
-			'--queue', '-q',
-			'--raw', '-C', -- insert command
-		}
-		local sub_prefix = '<barra_espaco>' -- Removendo '\ '
-		cmd = fn.substitute(cmd, [[\\\s]], sub_prefix, 'g')
-		local args = fn.split(cmd, [[\s]])
-		for i, arg in ipairs(args) do -- Recolocando '\ '
-			args[i] = fn.substitute(arg, sub_prefix, [[\\ ]], 'g')
-		end
-		local n = table.getn(args)
-		if lead == '' then -- Quando estiver dando entrada em novo argumento
-			-- Verificar se existe a opção de 'queue'
-			if args[n] == '--queue' or args[n] == '-q' then
-				local dirs = {}
-				local ls = fn.systemlist({'ls', DIR_MUSICAS})
-				table.insert(dirs, DIR_MUSICAS)
-				for _, diretorio in ipairs(ls) do -- adicionar '~/' no início de todas as opções 
-					table.insert(dirs, DIR_MUSICAS .. diretorio)
-				end
-				return dirs
-			end
-			return cmus_options
-		end
-		if args[n - 1] == '--queue' or args[n - 1] == '-q' then -- Entrada parcial para '--queue'
-			local dirs = {}
-			local ls = fn.systemlist({'fd', '--glob', '--full-path', '--type', 'directory', args[n] .. '*', DIR_MUSICAS})
-			for _, diretorio in ipairs(ls) do -- adicionar '~/'
-				table.insert(dirs, escapespace(diretorio))
-			end
-			return dirs
-		end
-		if fn.match(lead, '^-') >= 0 then -- Entrada de novo comando
-			local prompt = {}
-			for _, argumento in ipairs(cmus_options) do
-				if fn.match(argumento, lead) >= 0 then
-					table.insert(prompt, argumento)
-				end
-			end
-			return prompt
-		end
-		-- TODO: Melhorar lógica para sugestões
-		return cmus_options -- padrão
-	end
+	complete = FILLME
 })
