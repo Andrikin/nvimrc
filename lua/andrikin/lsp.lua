270d5ef49addd5907f5b74b4365f5482a6966cba
Andrikin <contasecretaalternativa@gmail.com>
2024-01-06 11:43:55 -0300

    refac: ajuste de configuracoes
    Injeção de nvim-cmp capabilities na inicialização dos servidores LSP:
    Python
    Deno

--- lua/andrikin/lsp.lua
+++ lua/andrikin/lsp.lua
@@ -3,44 +3,124 @@
 -- LSP SERVERS 
 -- TODO: Sempre instalar os arquivos binários dos executáveis
 local lsp = require('lspconfig')
+
+-- nvim-cmp
+local cmp = require('cmp')
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<C-n>'] = cmp.mapping.select_next_item(),
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		-- { name = 'vsnip' }, -- For vsnip users.
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+		}, {
+			{ name = 'buffer' },
+	})
+})
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+	sources = cmp.config.sources({
+		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+		}, {
+			{ name = 'buffer' },
+	})
+})
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+		}, {
+			{ name = 'cmdline' }
+	})
+})
+-- Set up lspconfig.
+local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+-- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+--   capabilities = capabilities
+-- }
+
 -- Python LSP
-lsp.pyright.setup({})
+lsp.pyright.setup({
+	capabilities = cmp_capabilities
+})
+
 -- HTML LSP
 lsp.html.setup({})
+
 -- Javascript LSP
-lsp.denols.setup({})
+lsp.denols.setup({
+	capabilities = cmp_capabilities
+})
+
 -- Lua LSP
-lsp.lua_ls.setup(
-	{
-		settings = {
-			Lua = {
-				runtime = {
-					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-					version = 'LuaJIT',
-				},
-				diagnostics = {
-					-- Get the language server to recognize the `vim` global
-					globals = {'vim'},
-				},
-				workspace = {
+lsp.lua_ls.setup({
+	on_init = function(client)
+		local path = client.workspace_folders[1].name
+		if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+			client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+				Lua = {
+					runtime = {
+						-- Tell the language server which version of Lua you're using
+						-- (most likely LuaJIT in the case of Neovim)
+						version = 'LuaJIT'
+					},
 					-- Make the server aware of Neovim runtime files
-					library = vim.api.nvim_get_runtime_file("", true),
-					checkThirdParty = false,
-				},
-				-- Do not send telemetry data containing a randomized but unique identifier
-				telemetry = {
-					enable = false,
-				},
-			},
-		},
-	}
-)
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME
+							-- "${3rd}/luv/library"
+							-- "${3rd}/busted/library",
+						}
+						-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+						-- library = vim.api.nvim_get_runtime_file("", true)
+					}
+				}
+			})
+			client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+		end
+		return true
+	end
+})
+
 -- lsp.vimls.setup({})
+
 -- lsp.rust_analyzer.setup({})
 
 require('colorizer').setup(nil, { css = true })
 
-require('nvim-treesitter.install').compilers = {'clang', 'gcc'}
+-- require('nvim-treesitter.install').compilers = {'clang', 'gcc'}
 require('nvim-treesitter.configs').setup{
 	highlight = {
 		enable = true,
@@ -49,11 +129,19 @@ require('nvim-treesitter.configs').setup{
 	indent = {
 		enable = true
 	},
-	-- ensure_installed = { -- linguagens para web development
-	-- 	'css', 'html', 'javascript',
-	-- 	'lua', 'python', 'scss',
-	-- 	'typescript', 'vim', 'vue', 'java', 
-	-- },
+	ensure_installed = { -- linguagens para web development
+		'css', 'html', 'javascript', 'vue',
+		'diff',
+		'git_config', 'git_rebase', 'gitattributes', 'gitcommit', 'gitignore',
+		'jsdoc', 'json', 'json5',
+		'lua', 'luadoc', 'luap', 'luau',
+		'markdown', 'markdown_inline',
+		'regex',
+		'xml',
+		'python',
+		'vim', 'java',
+		'latex',
+	},
 	context_commentstring = {
 		enable = true,
 	},
@@ -118,78 +206,6 @@ require('telescope').setup{
 			},
 		},
 	}
-}
-
-local cmp = require('cmp')
-cmp.setup({
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-		end,
-	},
-	window = {
-		-- completion = cmp.config.window.bordered(),
-		-- documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		['<C-n>'] = cmp.mapping.select_next_item(),
-		['<C-p>'] = cmp.mapping.select_prev_item(),
-	}),
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		-- { name = 'vsnip' }, -- For vsnip users.
-		-- { name = 'luasnip' }, -- For luasnip users.
-		-- { name = 'ultisnips' }, -- For ultisnips users.
-		-- { name = 'snippy' }, -- For snippy users.
-		}, {
-			{ name = 'buffer' },
-	})
-})
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-	sources = cmp.config.sources({
-		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-		}, {
-			{ name = 'buffer' },
-	})
-})
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = 'buffer' }
-	}
-})
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = 'path' }
-		}, {
-			{ name = 'cmdline' }
-	})
-})
--- Set up lspconfig.
-local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-require('lspconfig')['pyright'].setup {
-	capabilities = cmp_capabilities
-}
--- require('lspconfig')['rust_analyzer'].setup {
---   capabilities = capabilities
--- }
-require('lspconfig')['denols'].setup {
-	capabilities = cmp_capabilities
 }
 
 -- Mensagem de erro mais curta
