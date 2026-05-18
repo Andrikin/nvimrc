c21cc09f3b3ec9f8f6309ffd684fb87a7207f1b9
Andrikin <contasecretaalternativa@gmail.com>
2026-05-18 16:57:53 -0300

    remove files for new gvim configuration

--- lua/andrikin/init.lua
+++
@@ -1,8 +0,0 @@
--- Chamar todos os arquivos
-require('andrikin.lazy')
-require('andrikin.lsp')
-require('andrikin.options')
-require('andrikin.maps')
-require('andrikin.commands')
-require('andrikin.autocmds')
-
