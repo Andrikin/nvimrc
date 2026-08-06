4f0dcf591f921d08f3d787ce01f34737fc286604
Andrikin <contasecretaalternativa@gmail.com>
2026-08-06 00:07:06 -0300

    fix(autocmds): 'cursorline' and ui2 initialization

--- lua/andrikin/autocmds.lua
+++ lua/andrikin/autocmds.lua
@@ -11,14 +11,14 @@ autocmd('InsertEnter', {
         if dirvish then
             return
         end
-        vim.wo[0][0].cursorline = true
+        vim.wo.cursorline = true
     end,
 })
 autocmd('WinEnter', {
     group = Andrikin,
     pattern = '*',
     callback = function()
-        vim.wo[0][0].cursorline = false
+        vim.wo.cursorline = false
     end,
 })
 autocmd('InsertLeave', {
@@ -29,7 +29,7 @@ autocmd('InsertLeave', {
         if dirvish then
             return
         end
-        vim.wo[0][0].cursorline = false
+        vim.wo.cursorline = false
     end,
 })
 
@@ -67,33 +67,30 @@ autocmd( 'LspAttach', {
 	end
 })
 
-autocmd('VimEnter', {
-	group = Andrikin,
-	callback = function()
-		-- experimental: ui2
-		require('vim._core.ui2').enable()
-	end,
+autocmd('UIEnter', {
+    group = Andrikin,
+    -- experimental: ui2
+    callback = require('vim._core.ui2').enable
 })
 
--- WIP: obter o pid do processo "server"
-autocmd('VimEnter',{
-	group = Andrikin,
-	callback = function()
-		return -- remover depois de refeita a função
-		if vim.fn.executable("wmctrl") == 1 then
-			local winid = vim.fn.split(vim.system({
-				'wmctrl', '-pl', '|', 'grep',
-				vim.fn.shellescape(vim.fn.getpid())
-			}):wait().stdout)
-			winid = winid[1]
-			if not winid then
-				error('Não foi possível redimentsionar neovim.')
-			end
-			vim.system({
-				'wmctrl', '-i', '-b', 'add,maximized_vert,maximized_horz',
-				'-r', winid
-			})
-		end
-	end
-})
-
+-- -- WIP: obter o pid do processo "server"
+-- autocmd('VimEnter',{
+-- 	group = Andrikin,
+-- 	callback = function()
+-- 		if vim.fn.executable("wmctrl") == 1 then
+-- 			local winid = vim.fn.split(vim.system({
+-- 				'wmctrl', '-pl', '|', 'grep',
+-- 				vim.fn.shellescape(vim.fn.getpid())
+-- 			}):wait().stdout)
+-- 			winid = winid[1]
+-- 			if not winid then
+-- 				error('Não foi possível redimentsionar neovim.')
+-- 			end
+-- 			vim.system({
+-- 				'wmctrl', '-i', '-b', 'add,maximized_vert,maximized_horz',
+-- 				'-r', winid
+-- 			})
+-- 		end
+-- 	end
+-- })
+--
